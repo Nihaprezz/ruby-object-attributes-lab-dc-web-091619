38cacd1a3b401a3102ea86094ef2560926a6af8f
Sergio Perez <sergio13prez@gmail.com>
2019-09-17 15:00:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,5 @@
 class Dog
+<<<<<<< HEAD
     def name=(dogs_name) #setter
       @name = dogs_name
     end
@@ -33,3 +34,13 @@ class Person
         @job
     end
 end
+=======
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+
+  def name
+    @this_dogs_name
+  end
+end
+>>>>>>> 1677e79410fc92871cee8182c0e459f9afff71c1
